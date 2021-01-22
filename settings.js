8bed5212b316dbf662219e60abdd6a0f97525e3e
Damian Flynn <info@damianflynn.com>
2021-01-22 09:49:27 +0000

    Merging in 'natemason'

--- settings.js
+++ settings.js
@@ -9,21 +9,23 @@ exports.cbusname = "HOME";
 //mqtt server ip:port
 exports.mqtt = '127.0.0.1:1883';
 
+exports.topicPrefix = "homeassistant"
+exports.enableHassDiscovery = true;
 //username and password (unncomment to use)
-//exports.mqttusername = 'user1';
-//exports.mqttpassword = 'password1';
+exports.mqttusername = 'cbus';
+exports.mqttpassword = 'Password!';
 
 // net and app for automatically requesting values
-// exports.getallnetapp = '254/56';
+exports.getallnetapp = '254/56';
 
 // whether to request on start (requires getallnetapp set as well)
-// exports.getallonstart = true;
+exports.getallonstart = true;
 
 // how often to request after start (in seconds), (requires getallnetapp set as well)
-// exports.getallperiod = 60*60;
+exports.getallperiod = 60*60;
 
 // Sets MQTT retain flag for values coming from cgate
-// exports.retainreads = true;
+exports.retainreads = true;
 
 exports.messageinterval = 200;
 
